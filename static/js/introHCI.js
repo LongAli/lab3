4165b2aacbfa91111617b97706e78eb54eed2215
Ali L <alil@alis-mbp-6.lan>
2021-01-21 10:24:45 -0800

    added Javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,16 +11,25 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$('#testjs').text("Please wait...");
+		$(".jumbotron p").addClass("active");
 	});
 
 	$("a.thumbnail").click(projectClick);
+
 }
 
 function projectClick(e){
 	e.preventDefault();
 
 	var containingProject = $(this).closest(".project");
-    containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+			 $(".project-description").fadeOut();
+    }
 }
 
 
